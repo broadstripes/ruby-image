db4f74114f8f62598ebf8c96b04b208dcc53ca3c
Javier Maldonado <3goliad@gmail.com>
2025-09-11 17:06:55 -0400

    improve caching by reordering build
    useful packages should be added last, as they're most likely to change

--- install_useful_packages.sh
+++ install_useful_packages.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+export DEBIAN_FRONTEND=noninteractive;
+apt-get update
 apt-get -y --no-install-recommends install \
     curl \
     file \
@@ -11,3 +13,9 @@ apt-get -y --no-install-recommends install \
     nano \
     procps \
     less
+
+if [[ $(. /etc/os-release; echo "$VERSION_ID") -ge 13 ]]; then
+    apt-get distclean
+else
+    rm -rf /var/lib/apt/lists/*
+fi
